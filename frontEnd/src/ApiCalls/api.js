8d45a95a9de6d8ced2429994171fdace7768faf4
Sahil <hanss9545@gmail.com>
2023-11-27 19:11:59 +0530

    cors add origin

--- frontEnd/src/ApiCalls/api.js
+++ frontEnd/src/ApiCalls/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getUser = (username) => {
   return axios
-    .get(`http://localhost:5000/api/getUser/${username}`)
+    .get(`https://megatalkbackend.onrender.com/api/getUser/${username}`)
     .then((response) => {
       // Handle the successful response
       // console.log("salu", response.data);
