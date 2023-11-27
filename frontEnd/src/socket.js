24eff9757b1b3bb672a72da38f42b843903662cd
Sahil Hans <81958229+sahilHans9545@users.noreply.github.com>
2023-11-28 00:29:11 +0530

    Update socket.js

--- frontEnd/src/socket.js
+++ frontEnd/src/socket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("https://megatalk.onrender.com");
+const socket = io("https://megatalk.onrender.com",{
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
 export default socket;
