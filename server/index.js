a08e063eed77be79c9dc988e2de2ee6fb9644453
Sahil Hans <81958229+sahilHans9545@users.noreply.github.com>
2023-11-28 00:28:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ const server = app.listen(5000, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://megatalk.onrender.com",
+    origin: ["http://localhost:5173/", "https://megatalk.onrender.com/"],
   },
 });
 
